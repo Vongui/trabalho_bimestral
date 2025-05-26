4a798e665b607dea27ad3209d8deaf779c536c5e
Vongui <guilhermemoraes8@hotmail.com>
2025-05-26 10:27:24 -0300

    feat: add modelos do banco

--- model/cliente.py
+++ model/cliente.py
@@ -11,7 +11,7 @@ class Cliente():
         self._lista = 'nome, endereco, cidade, uf, cep'
         self.__dadosInserir = ""
         self.__dadosUpdate = ""
-        self.__dadosUpdate = ""
+        self.__dadosDelete = ""
         self.__dadosPesquisa = ""
         self.__tabelaBanco = "cliente"
 
@@ -19,3 +19,14 @@ class Cliente():
     def dadosInserir(self):
         self.__dadosInserir = f"'{self._nome}', '{self._endereco}', '{self._cidade}', '{self._uf}', '{self._cep}'"
         return self.__dadosInserir
+
+    @property
+    def dadosUpdate(self):
+        self.__dadosUpdate = (("nome='{}',endereco='{}',cidade='{}',uf='{}',cep='{}' where idcliente='{}'")
+                              .format(self._nome, self._endereco, self._cidade, self._uf, self._cep, self._idcliente))
+        return self.__dadosUpdate
+
+    @property
+    def dadosDelete(self):
+        self.__dadosDelete = "where idcliente='{}'".format(self._idcliente)
+        return self.__dadosDelete
